564c4c4c0406bd09fa5f80d1db0ff9b5807a376a
vitJR1 <kalugin.vitaliy23@gmail.com>
2024-08-07 08:35:42 +0500

    feat: ServiceRequestTimeoutPipe

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, UseFilters } from '@nestjs/common';
+import { Controller, UseFilters, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -6,12 +6,16 @@ import { DefaultFilter } from '../../utils/default.filter';
 import { User } from './entities/user.entity';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ID } from '../../utils/ID';
-import { ExceptionFilter } from '@pieceowater-dev/lotof.lib.broadcaster';
+import {
+  ExceptionFilter,
+  ServiceRequestTimeoutPipe,
+} from '@pieceowater-dev/lotof.lib.broadcaster';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(new ServiceRequestTimeoutPipe())
   @UseFilters(new ExceptionFilter())
   @MessagePattern('createUser')
   create(@Payload() data: CreateUserDto) {
